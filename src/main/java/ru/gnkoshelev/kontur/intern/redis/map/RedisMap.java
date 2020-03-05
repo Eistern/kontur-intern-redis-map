21589420c685e7107ef13d755e5e1edd613c4f4c
d.zulin <d.zulin@g.nsu.ru>
2020-03-05 19:12:27 +0700

    containsKey, put implemented

--- src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
+++ src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
@@ -1,70 +1,108 @@
 package ru.gnkoshelev.kontur.intern.redis.map;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
 
 /**
  * @author Gregory Koshelev
  */
 public class RedisMap implements Map<String,String> {
-    @Override
-    public int size() {
-        throw new UnsupportedOperationException();
+  private static volatile JedisPool jedisPool = new JedisPool();
+  private final String mapId;
+
+  {
+    try (Jedis jedisConnection = jedisPool.getResource()){
+      this.mapId = "redis-map:" + jedisConnection.incr("redis-map-id-generator").toString();
+    }
+  }
+
+  @Override
+  public int size() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    if (key == null)
+      throw new NullPointerException();
+
+    if (!(key instanceof String))
+      throw new ClassCastException();
+
+    Boolean result;
+    try (Jedis jedisConnection = jedisPool.getResource()){
+       result = jedisConnection.hexists(mapId, (String) key);
     }
 
-    @Override
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+    return result;
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String get(Object key) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String put(String key, String value) {
+    String result = null;
+    try (Jedis jedisConnection = jedisPool.getResource()) {
+      Transaction transaction = jedisConnection.multi();
+
+      Response<String> previousValue = transaction.hget(mapId, key);
+      Response<Long> checkResponse = transaction.hset(mapId, key, value);
+
+      transaction.exec();
+
+      if (checkResponse.get() == 0L)
+        result = previousValue.get();
     }
 
-    @Override
-    public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException();
-    }
+    return result;
+  }
 
-    @Override
-    public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public String remove(Object key) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public String get(Object key) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void putAll(Map<? extends String, ? extends String> m) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public String put(String key, String value) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void clear() {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public String remove(Object key) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public Set<String> keySet() {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public void putAll(Map<? extends String, ? extends String> m) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public Collection<String> values() {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Set<String> keySet() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<String> values() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Set<Entry<String, String>> entrySet() {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public Set<Entry<String, String>> entrySet() {
+    throw new UnsupportedOperationException();
+  }
 }
--- src/test/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMapTest.java
+++ src/test/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMapTest.java
@@ -11,35 +11,35 @@ import java.util.Set;
  * @author Gregory Koshelev
  */
 public class RedisMapTest {
-    @Test
-    public void baseTests() {
-        Map<String, String> map1 = new RedisMap();
-        Map<String, String> map2 = new RedisMap();
+  @Test
+  public void baseTests() {
+    Map<String, String> map1 = new RedisMap();
+    Map<String, String> map2 = new RedisMap();
 
-        map1.put("one", "1");
+    map1.put("one", "1");
 
-        map2.put("one", "ONE");
-        map2.put("two", "TWO");
+    map2.put("one", "ONE");
+    map2.put("two", "TWO");
 
-        Assert.assertEquals("1", map1.get("one"));
-        Assert.assertEquals(1, map1.size());
-        Assert.assertEquals(2, map2.size());
+    Assert.assertEquals("1", map1.get("one"));
+    Assert.assertEquals(1, map1.size());
+    Assert.assertEquals(2, map2.size());
 
-        map1.put("one", "first");
+    map1.put("one", "first");
 
-        Assert.assertEquals("first", map1.get("one"));
-        Assert.assertEquals(1, map1.size());
+    Assert.assertEquals("first", map1.get("one"));
+    Assert.assertEquals(1, map1.size());
 
-        Assert.assertTrue(map1.containsKey("one"));
-        Assert.assertFalse(map1.containsKey("two"));
+    Assert.assertTrue(map1.containsKey("one"));
+    Assert.assertFalse(map1.containsKey("two"));
 
-        Set<String> keys2 = map2.keySet();
-        Assert.assertEquals(2, keys2.size());
-        Assert.assertTrue(keys2.contains("one"));
-        Assert.assertTrue(keys2.contains("two"));
+    Set<String> keys2 = map2.keySet();
+    Assert.assertEquals(2, keys2.size());
+    Assert.assertTrue(keys2.contains("one"));
+    Assert.assertTrue(keys2.contains("two"));
 
-        Collection<String> values1 = map1.values();
-        Assert.assertEquals(1, values1.size());
-        Assert.assertTrue(values1.contains("first"));
-    }
+    Collection<String> values1 = map1.values();
+    Assert.assertEquals(1, values1.size());
+    Assert.assertTrue(values1.contains("first"));
+  }
 }
